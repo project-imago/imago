368d41c860fc3fac364d3f1af35bdfbd7d688e10
Pierre de Lacroix <pierre@pdelacroix.com>
2020-06-29 17:06:18 +0200

    fixes

--- config/dev.exs
+++ config/dev.exs
@@ -87,10 +87,10 @@ config :imago, ImagoWeb.Endpoint,
 #   root_iri: "http://imago.local"
 
 config :imago, Imago.Graph,
-  endpoint: "http://wdqs.imago.local:9999/bigdata/namespace/wdq/sparql/wdq/sparql"
+  endpoint: "http://wdqs:9999/bigdata/namespace/wdq/sparql"
 
 config :matrix_app_service,
-  path: "/matrix",
+  path: "/api/matrix",
   base_url: "http://matrix.imago.local:8008",
   access_token: "MDAyMGxvY2F0aW9uIG1hdHJpeC5pbWFnby5sb2NhbAowMDEzaWRlbnRpZmllciBrZXkKMDAxMGNpZCBnZW4gPSAxCjAwMmNjaWQgdXNlcl9pZCA9IEBhbGljZTptYXRyaXguaW1hZ28ubG9jYWwKMDAxNmNpZCB0eXBlID0gYWNjZXNzCjAwMjFjaWQgbm9uY2UgPSAjRC5ieWJrMEUuMU0qT0xJCjAwMmZzaWduYXR1cmUg-jGyjY9CK07mRt1h4p_86D6SJr1ZqrGr8YlsIW_jLtMK",
   homeserver_token: "MDAyMGxvY2F0aW9uIG1hdHJpeC5pbWFnby5sb2NhbAowMDEzaWRlbnRpZmllciBrZXkKMDAxMGNpZCBnZW4gPSAxCjAwMmNjaWQgdXNlcl9pZCA9IEBhbGljZTptYXRyaXguaW1hZ28ubG9jYWwKMDAxNmNpZCB0eXBlID0gYWNjZXNzCjAwMjFjaWQgbm9uY2UgPSBjcX4jazVTUDNeUlk2WnRECjAwMmZzaWduYXR1cmUg_K2biF-xm5ue7985RkAomVadF7yfy3UiEpH-e15m0esK"
