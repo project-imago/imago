bf8b065068904051d7c06c5c4facead3667b694e
Pierre de Lacroix <pierre@pdelacroix.com>
2020-07-27 17:43:26 +0200

    fix endpoint url

--- config/releases.exs
+++ config/releases.exs
@@ -16,6 +16,12 @@ config :imago, Imago.Repo,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
+endpoint_url =
+  System.get_env("ENDPOINT_URL") ||
+    raise """
+    environment variable ENDPOINT_URL is missing.
+    """
+
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
     raise """
@@ -25,6 +31,7 @@ secret_key_base =
 
 config :imago, ImagoWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  url: [host: endpoint_url, port: 443],
   secret_key_base: secret_key_base
 
 wdqs_url =
